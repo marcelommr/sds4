9e4bc4326f25f3f546226a5d7790e5b2972c32cd
Marcelo <marcelomrtrabalho@gmail.com>
2021-09-12 21:58:01 -0300

    Basic static componenets

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,83 @@
+
+
+const DataTable = () => {
+    return (
+      
+      <footer className="footer mt-auto py-3 bg-white">
+      <div className="table-responsive">
+    <table className="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th>Data</th>
+                <th>Vendedor</th>
+                <th>Clientes visitados</th>
+                <th>Negócios fechados</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+    </footer>
+      
+    );
+  }
+  
+  export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+
+
+const Footer = () => {
+    return (
+      
+      <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/marcelomresende" target="_blank" rel="noreferrer">Marcelo</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+          Evento promovido pela escola DevSuperior: <a href="https://instagram.com/mascelo" target="_blank" rel="noreferrer">@mascelo</a></small></p>
+      </div>
+    </footer>
+      
+    );
+  }
+  
+  export default Footer;
