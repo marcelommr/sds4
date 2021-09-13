157de7dbc1d82e5341a3640b99a0ec3fe8a65062
Marcelo <marcelomrtrabalho@gmail.com>
2021-09-13 01:58:07 -0300

    fix heroku with gradlew

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -1,32 +1,13 @@
 package com.devsuperior.dsvendas;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 
-import java.net.InetAddress;
-
-@Slf4j
 @SpringBootApplication
 public class DsvendasApplication {
 
-	private static final String ACCESS_URLS_MESSAGE_LOG =
-			"\n\n Access URLs:\n----------------------------------------------------------\n\t External: \thttp://{}:{}/sellers\n----------------------------------------------------------\n";
-
-	public static void main(final String[] args) {
-		try {
-			System.setProperty("spring.devtools.restart.enabled", "false");
-			final SpringApplication app = new SpringApplication(DsvendasApplication.class);
-			final Environment env = app.run().getEnvironment();
-			log.info(
-					ACCESS_URLS_MESSAGE_LOG,
-					InetAddress.getLocalHost().getHostAddress(),
-					env.getProperty("server.port"));
-		} catch (Exception e) {
-			log.error("Start Error.", e);
-		}
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
 	}
-
 
 }
