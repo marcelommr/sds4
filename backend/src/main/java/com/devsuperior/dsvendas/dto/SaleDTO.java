906f3aed0d87967e39587709755bbb75cf9b6d7b
Marcelo <marcelomrtrabalho@gmail.com>
2021-09-13 00:20:05 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dsvendas.dto;
+
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.entities.Seller;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+public class SaleDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    private SellerDTO seller;
+
+    public SaleDTO(Sale entity){
+        id = entity.getId();
+        visited = entity.getVisited();
+        deals = entity.getDeals();
+        amount = entity.getAmount();
+        date = entity.getDate();
+        seller = new SellerDTO(entity.getSeller());
+    }
+
+}
